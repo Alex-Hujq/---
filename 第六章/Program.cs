976dff1bcebc4804a3b259ac592d6ccc194dc9ce
94987 <94987@LAPTOP-HP185DJF>
2019-10-05 10:48:03 +0800

    第六章一半

--- 第六章/Program.cs
+++ 第六章/Program.cs
@@ -45,6 +45,19 @@ namespace 第六章
             {
                 Console.WriteLine(intArray.GetValue(i));
             }
+
+            int[][] jagged = new int[3][];
+            jagged[0] = new int[2] { 1, 2 };
+            jagged[1] = new int[6] { 3, 4, 5, 6, 7, 8 };
+            jagged[2] = new int[3] { 9, 10, 11 };
+
+            for(int row=0;row<jagged.Length;row++)
+            {
+                for(int element=0;element<jagged[row].Length;element++)
+                {
+                    Console.WriteLine("row:{0},element:{1},value:{2}", row, element, jagged[row][element]);
+                }
+            }
         }
     }
 }
