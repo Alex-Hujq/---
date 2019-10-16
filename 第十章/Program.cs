bc38e75d5a3a0345b9f8d0c79ce670ac4e056c2e
94987 <94987@LAPTOP-HP185DJF>
2019-10-16 23:37:20 +0800

    305进11章

--- 第十章/Program.cs
+++ 第十章/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Threading;
+using System.Linq;
+using System.Collections.ObjectModel;
 
 namespace 第十章
 {
@@ -130,6 +132,165 @@ namespace 第十章
             {
                 Console.WriteLine(title);
             }
+            string isbn1;
+            string title1 = "Professional c# 7.0";
+            if(!books.TryGetValue(title1,out isbn1))
+            {
+                Console.WriteLine("{0} not found", title1);
+            }
+            //
+            var employees = new Dictionary<EmployeeId, Employee>(31);
+
+            var idTony = new EmployeeId("C3755");
+            var tony = new Employee(idTony, "Tony Stewart", 379025.00m);
+            employees.Add(idTony, tony);
+            Console.WriteLine(tony);
+
+            var idCarl = new EmployeeId("F3547");
+            var carl = new Employee(idCarl, "Carl Edwards", 403466.00m);
+            employees.Add(idCarl, carl);
+            Console.WriteLine(carl);
+
+            var idKevin = new EmployeeId("C3386");
+            var kevin = new Employee(idKevin, "Kevin Harwick", 415261.00m);
+            employees.Add(idKevin, kevin);
+            Console.WriteLine(kevin);
+
+            var idMatt = new EmployeeId("F3323");
+            var matt = new Employee(idMatt, "Matt Kenseth", 1589390.00m);
+            employees[idMatt] = matt;
+            Console.WriteLine(matt);
+
+            var idBrad = new EmployeeId("D3234");
+            var brad = new Employee(idBrad, "Brad Keselowski", 322295.00m);
+            employees[idBrad] = brad;
+            Console.WriteLine(brad);
+
+
+
+            while (true)
+            {
+                Console.Write("Enter employee id (X to exit)> ");
+                var userInput = Console.ReadLine();
+                userInput = userInput.ToUpper();
+                if (userInput == "X") break;
+
+                EmployeeId id;
+                try
+                {
+                    id = new EmployeeId(userInput);
+
+
+                    Employee employee;
+                    if (!employees.TryGetValue(id, out employee))
+                    {
+                        Console.WriteLine("Employee with id {0} does not exist", id);
+                    }
+                    else
+                    {
+                        Console.WriteLine(employee);
+                    }
+                }
+                catch (EmployeeIdException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            //字典
+            var racers1 = new List<Racer>();
+            racers.Add(new Racer(26, "Jacques", "Villeneuve", "Canada", 11));
+            racers.Add(new Racer(18, "Alan", "Jones", "Australia", 12));
+            racers.Add(new Racer(11, "Jackie", "Stewart", "United Kingdom", 27));
+            racers.Add(new Racer(15, "James", "Hunt", "United Kingdom", 10));
+            racers.Add(new Racer(5, "Jack", "Brabham", "Australia", 14));
+
+            var lookupRacers = racers.ToLookup(r => r.Country);
+
+            foreach (Racer r in lookupRacers["Australia"])
+            {
+                Console.WriteLine(r);
+            }
+
+            //lookup
+            var companyTeams = new HashSet<string>() { "Ferrari", "McLaren", "Mercedes" };
+            var traditionalTeams = new HashSet<string>() { "Ferrari", "McLaren" };
+            var privateTeams = new HashSet<string>() { "Red Bull", "Lotus", "Toro Rosso", "Force India", "Sauber" };
+
+            if (privateTeams.Add("Williams"))
+                Console.WriteLine("Williams added");
+            if (!companyTeams.Add("McLaren"))
+                Console.WriteLine("McLaren was already in this set");
+
+            if (traditionalTeams.IsSubsetOf(companyTeams))
+            {
+                Console.WriteLine("traditionalTeams is subset of companyTeams");
+            }
+
+            if (companyTeams.IsSupersetOf(traditionalTeams))
+            {
+                Console.WriteLine("companyTeams is a superset of traditionalTeams");
+            }
+
+
+            traditionalTeams.Add("Williams");
+            if (privateTeams.Overlaps(traditionalTeams))
+            {
+                Console.WriteLine("At least one team is the same with the traditional " +
+                      "and private teams");
+            }
+
+            var allTeams = new SortedSet<string>(companyTeams);
+            allTeams.UnionWith(privateTeams);
+            allTeams.UnionWith(traditionalTeams);
+
+            Console.WriteLine();
+            Console.WriteLine("all teams");
+            foreach (var team in allTeams)
+            {
+                Console.WriteLine(team);
+            }
+
+            allTeams.ExceptWith(privateTeams);
+            Console.WriteLine();
+            Console.WriteLine("no private team left");
+            foreach (var team in allTeams)
+            {
+                Console.WriteLine(team);
+            }
+            //集
+            var data = new ObservableCollection<string>();
+            data.CollectionChanged += Data_CollectionChanged;
+            data.Add("One");
+            data.Add("Two");
+            data.Insert(1, "Three");
+            data.Remove("One");
+
+        }
+
+        static void Data_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            Console.WriteLine("action: {0}", e.Action.ToString());
+
+            if (e.OldItems != null)
+            {
+                Console.WriteLine("starting index for old item(s): {0}", e.OldStartingIndex);
+                Console.WriteLine("old item(s):");
+                foreach (var item in e.OldItems)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            if (e.NewItems != null)
+            {
+                Console.WriteLine("starting index for new item(s): {0}", e.NewStartingIndex);
+                Console.WriteLine("new item(s): ");
+                foreach (var item in e.NewItems)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.WriteLine();
+            //可观察的数组
 
         }
     }
---
+++ 第十章/employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 第十章
+{
+    [Serializable]
+    public class Employee
+    {
+        private string name;
+        private decimal salary;
+        private readonly EmployeeId id;
+
+        public Employee(EmployeeId id, string name, decimal salary)
+        {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}: {1, -20} {2:C}",
+                  id.ToString(), name, salary);
+        }
+    }
+}
